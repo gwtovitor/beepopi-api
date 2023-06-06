4eb6ca9617d146f8dadf4c59534bf7c0cb5b8086
jds <joseraldom@gmail.com>
2023-06-05 22:04:03 -0400

    correcao de verificacao ao criar usuario

--- src/middleware/globalMiddleware.js
+++ src/middleware/globalMiddleware.js
@@ -3,10 +3,9 @@ import { findUserById, findUserByUsername, findUserByEmail, findUserByCpf } from
 const verifyUserToSave = async (req, res, next) => {
   try {
 
-    console.log(req.body)
     const { email, username, password, cpf } = req.body;
 
-    if (!email || !username || !password || !cpf) return res.status(400).send({ message: "Todos os campos devem ser preenchidos" });
+    if (!email || !username || !password) return res.status(400).send({ message: "Todos os campos devem ser preenchidos" });
 
     if (await findUserByEmail(email)) return res.status(400).send({ message: "Email já está sendo usado." });
 
