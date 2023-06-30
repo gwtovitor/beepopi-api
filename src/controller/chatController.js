6898c7a914039a9bd8ce33a522776808893eaeea
jds <joseraldom@gmail.com>
2023-06-30 08:56:18 -0400

    tentativa de correcao do socket

--- src/controller/chatController.js
+++ src/controller/chatController.js
@@ -1,19 +1,25 @@
 import { createChat, findChatByUsers } from '../service/chatService.js';
 import { Server } from 'socket.io';
+import cors from 'cors';
 
 export const chat = (server) => {
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: { origin: '*' }
+  });
 
   global.onlineUsers = new Map();
   io.on("connection", (socket) => {
+    console.log("connection")
     global.chatSocket = socket;
     socket.on("add-user", (userId) => {
       onlineUsers.set(userId, socket.id);
     });
 
     socket.on("send-msg", (data) => {
+      console.log("send-msg")
       const sendUserSocket = onlineUsers.get(data.to);
       if (sendUserSocket) {
+        console.log(sendUserSocket)
         socket.to(sendUserSocket).emit("msg-recieve", data.msg);
       }
     });
@@ -24,11 +30,7 @@ export const getMessages = async (req, res, next) => {
   try {
     const { from, to } = req.body;
 
-    const messages = await findChatByUsers({
-      users: {
-        $all: [from, to],
-      },
-    }).sort({ updatedAt: 1 });
+    const messages = await findChatByUsers([from, to]);
 
     const projectedMessages = messages.map((msg) => {
       return {
